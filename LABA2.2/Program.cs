392b4ec2a213bbf7e36c77705c2ab37e5ee41714
Chloe3c <kristianishenko21@gmail.com>
2024-12-12 12:48:45 +0200

    Добавьте файлы проекта.

---
+++ LABA2.2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+if ((x > 0) && (y < 0)) Console.WriteLine("Точка находится в 4 четверти");
+else Console.WriteLine("Точка не находится в 4 четверти");
